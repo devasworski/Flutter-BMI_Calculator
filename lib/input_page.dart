3d475a06bfc943a2318372373e04b11c1bfcec89
Alexander Sworski <mail@dev-sworski.com>
2021-01-21 18:46:11 +0100

    Immutable

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,60 +1,62 @@
 import 'package:flutter/material.dart';
 
+const Color cardBoxColor = Color(0xFF323243);
+const Color bottomContainerColor = Color(0xFFD64C68);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
-  Container getBox() => Container(
-        margin: EdgeInsets.all(15),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10), color: Color(0xFF323243)),
-      );
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                      child: CardBox(
-                    color: Color(0xFF323243),
-                  )),
-                  Expanded(
-                      child: CardBox(
-                    color: Color(0xFF323243),
-                  )),
-                ],
-              ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                    child: CardBox(
+                  color: cardBoxColor,
+                )),
+                Expanded(
+                    child: CardBox(
+                  color: cardBoxColor,
+                )),
+              ],
             ),
-            Expanded(
-                child: CardBox(
-              color: Color(0xFF323243),
-            )),
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                      child: CardBox(
-                    color: Color(0xFF323243),
-                  )),
-                  Expanded(
-                      child: CardBox(
-                    color: Color(0xFF323243),
-                  )),
-                ],
-              ),
+          ),
+          Expanded(
+              child: CardBox(
+            color: cardBoxColor,
+          )),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                    child: CardBox(
+                  color: cardBoxColor,
+                )),
+                Expanded(
+                    child: CardBox(
+                  color: cardBoxColor,
+                )),
+              ],
             ),
-          ],
-        ),
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
+            height: 80,
+            child: Center(),
+          )
+        ],
       ),
     );
   }
