91a224e6c1fa7d3bbf74578d8351ada15fa6581b
Alexander Sworski <mail@dev-sworski.com>
2021-01-21 18:16:45 +0100

    Refactor Flutter Widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -6,15 +6,70 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Container getBox() => Container(
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10), color: Color(0xFF323243)),
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                      child: CardBox(
+                    color: Color(0xFF323243),
+                  )),
+                  Expanded(
+                      child: CardBox(
+                    color: Color(0xFF323243),
+                  )),
+                ],
+              ),
+            ),
+            Expanded(
+                child: CardBox(
+              color: Color(0xFF323243),
+            )),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                      child: CardBox(
+                    color: Color(0xFF323243),
+                  )),
+                  Expanded(
+                      child: CardBox(
+                    color: Color(0xFF323243),
+                  )),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class CardBox extends StatelessWidget {
+  CardBox({@required this.color});
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration:
+          BoxDecoration(borderRadius: BorderRadius.circular(10), color: color),
     );
   }
 }
